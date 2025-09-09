7c33a172f1e687f62fc9d5668d5f0659bfd8d561
Tahir <166417422+Tahir880@users.noreply.github.com>
2025-09-09 20:20:15 +0100

    Update layout.tsx

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -1,15 +1,13 @@
 import type {Metadata} from 'next';
 import {notFound} from 'next/navigation';
 import {routing} from '@/i18n/routing';
-
 import {setRequestLocale} from 'next-intl/server';
 import {NextIntlClientProvider} from 'next-intl';
 import {getMessages} from 'next-intl/server';
-
 import NavBar from '@/components/NavBar';
 import Footer from '@/components/Footer';
+import ErrorCatcher from '@/components/ErrorCatcher';
 import Script from 'next/script';
-
 import '@/app/globals.css';
 
 export const metadata: Metadata = {
@@ -35,16 +33,21 @@ export default async function RootLayout({
   }
 
   setRequestLocale(locale);
-
   const messages = await getMessages();
 
   return (
     <html lang={locale} dir={locale === 'ar' ? 'rtl' : 'ltr'}>
       <body>
         <NextIntlClientProvider messages={messages} locale={locale}>
-          <NavBar />
-          {children}
-          <Footer />
+          <ErrorCatcher name="NavBar">
+            <NavBar />
+          </ErrorCatcher>
+
+          <ErrorCatcher name="Content">{children}</ErrorCatcher>
+
+          <ErrorCatcher name="Footer">
+            <Footer />
+          </ErrorCatcher>
 
           <Script
             id="org-jsonld"
